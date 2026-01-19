b40f4879aeedb1f57feac366254202ebc7dd9e5a
murata-kaira <k024g0062@g.neec.ac.jp>
2026-01-19 15:23:42 +0900

    kokokokoko

--- main.cpp
+++ main.cpp
@@ -20,10 +20,10 @@
 #include <xaudio2.h>
 #define DIRECTINPUT_VERSION   0x0800 
 #include <dinput.h>
-#include "Input.h"
-#include "WinApp.h"
-#include "DirectXCommon.h"
-#include "StringUtility.h"
+#include "Engine/io/Input.h"
+#include "Engine/base/WinApp.h"
+#include "Engine/base/DirectXCommon.h"
+#include "Engine/base/StringUtility.h"
 
 #include "Vector2.h"
 #include "Vector4.h"
